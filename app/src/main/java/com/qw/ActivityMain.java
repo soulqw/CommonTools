c1a148e9e3cfd76c4e945fac72068922491d0230
george <cd5160866@126.com>
2022-09-17 00:24:17 +0800

    [ADD]add tlog

--- app/src/main/java/com/qw/ActivityMain.java
+++ app/src/main/java/com/qw/ActivityMain.java
@@ -1,8 +1,6 @@
 package com.qw;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,15 +9,10 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.module.GlideModule;
-import com.qw.tools.R;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import com.qw.tools.R;
+import com.test.soultools.tool.log.TLog;
+
 
 /**
  * Author: 思忆
@@ -36,40 +29,14 @@ public class ActivityMain extends AppCompatActivity {
         buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TLog.d("qw","123");
                 Toast.makeText(ActivityMain.this, "", Toast.LENGTH_SHORT).show();
             }
         });
 
-        startHook(buttonTest);
-        ImageView imageView = findViewById(R.id.iv_glide);
-        ImageView imageView2 = findViewById(R.id.iv_glide2);
-        Glide.with(this).load("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg")
-                .into(imageView);
-        Glide.with(this).load("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg")
-                .into(imageView2);
-    }
-
-    private void startHook(View view) {
-        try {
-            Method method = View.class.getDeclaredMethod("getListenerInfo");
-            method.setAccessible(true);
-            Object mListenerInfo = method.invoke(view);
-
-            Class<?> lisntenrClass = Class.forName("android.view.View$ListenerInfo");
-            Field field = lisntenrClass.getDeclaredField("mOnClickListener");
-            final View.OnClickListener onClickListener = (View.OnClickListener) field.get(mListenerInfo);
-            Object proxy = Proxy.newProxyInstance(view.getContext().getClassLoader(), new Class[]{View.OnClickListener.class}, new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    Log.d("qw", "this is from invoke");
-                    return method.invoke(onClickListener, args);
-                }
-            });
-            field.set(mListenerInfo, proxy);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
+
+
 
 }
---
+++ soultools/src/main/java/com/test/soultools/tool/log/TLog.java
@@ -0,0 +1,153 @@
+package com.test.soultools.tool.log;
+
+import android.util.Log;
+
+public class TLog {
+
+    private static final int MAX_LENGTH = 4000;
+    private static final String NULL_TIPS = "Log with null object";
+    private static final String PARAM = "Param";
+    private static final String NULL = "null";
+    private static final String SUFFIX = ".java";
+    private static final int V = 0x1;
+    private static final int D = 0x2;
+    private static final int I = 0x3;
+    private static final int W = 0x4;
+    private static final int E = 0x5;
+    private static final int A = 0x6;
+
+    private static final int STACK_TRACE_INDEX_5 = 5;
+
+    public static void d(Object msg) {
+        printLog(D, null, msg);
+    }
+
+    public static void d(String tag, Object... objects) {
+        printLog(D, tag, objects);
+    }
+
+    public static void i(Object msg) {
+        printLog(I, null, msg);
+    }
+
+    public static void i(String tag, Object... objects) {
+        printLog(I, tag, objects);
+    }
+
+    public static void w(Object msg) {
+        printLog(W, null, msg);
+    }
+
+    public static void w(String tag, Object... objects) {
+        printLog(W, tag, objects);
+    }
+
+    public static void e(Object msg) {
+        printLog(E, null, msg);
+    }
+
+    public static void e(String tag, Object... objects) {
+        printLog(E, tag, objects);
+    }
+
+    private static void printLog(int type, String tagStr, Object... objects) {
+        String[] contents = wrapperContent(STACK_TRACE_INDEX_5, tagStr, objects);
+        String tag = contents[0];
+        String msg = contents[1];
+        String headString = contents[2];
+        printDefault(type, tag, headString + msg);
+    }
+
+    private static String[] wrapperContent(int stackTraceIndex, String tagStr, Object... objects) {
+
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        StackTraceElement targetElement = stackTrace[stackTraceIndex];
+        String className = targetElement.getClassName();
+        String[] classNameInfo = className.split("\\.");
+        if (classNameInfo.length > 0) {
+            className = classNameInfo[classNameInfo.length - 1] + SUFFIX;
+        }
+
+        if (className.contains("$")) {
+            className = className.split("\\$")[0] + SUFFIX;
+        }
+
+        String methodName = targetElement.getMethodName();
+        int lineNumber = targetElement.getLineNumber();
+
+        if (lineNumber < 0) {
+            lineNumber = 0;
+        }
+
+        String tag = (tagStr == null ? className : tagStr);
+        String msg = (objects == null) ? NULL_TIPS : getObjectsString(objects);
+        String headString = "[ (" + className + ":" + lineNumber + ")#" + methodName + " ] ";
+
+        return new String[]{tag, msg, headString};
+    }
+
+    private static String getObjectsString(Object... objects) {
+
+        if (objects.length > 1) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("\n");
+            for (int i = 0; i < objects.length; i++) {
+                Object object = objects[i];
+                if (object == null) {
+                    stringBuilder.append(PARAM).append("[").append(i).append("]").append(" = ").append(NULL).append("\n");
+                } else {
+                    stringBuilder.append(PARAM).append("[").append(i).append("]").append(" = ").append(object.toString()).append("\n");
+                }
+            }
+            return stringBuilder.toString();
+        } else if (objects.length == 1) {
+            Object object = objects[0];
+            return object == null ? NULL : object.toString();
+        } else {
+            return NULL;
+        }
+    }
+
+    public static void printDefault(int type, String tag, String msg) {
+
+        int index = 0;
+        int length = msg.length();
+        int countOfSub = length / MAX_LENGTH;
+
+        if (countOfSub > 0) {
+            for (int i = 0; i < countOfSub; i++) {
+                String sub = msg.substring(index, index + MAX_LENGTH);
+                printSub(type, tag, sub);
+                index += MAX_LENGTH;
+            }
+            printSub(type, tag, msg.substring(index, length));
+        } else {
+            printSub(type, tag, msg);
+        }
+    }
+
+    private static void printSub(int type, String tag, String sub) {
+        switch (type) {
+            case V:
+                Log.v(tag, sub);
+                break;
+            case D:
+                Log.d(tag, sub);
+                break;
+            case I:
+                Log.i(tag, sub);
+                break;
+            case W:
+                Log.w(tag, sub);
+                break;
+            case E:
+                Log.e(tag, sub);
+                break;
+            case A:
+                Log.wtf(tag, sub);
+                break;
+        }
+    }
+
+}
