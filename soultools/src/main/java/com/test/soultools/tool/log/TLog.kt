06256939a6d8310536dd169f4d01cbb7654732dc
george <cd5160866@126.com>
2022-09-19 00:17:22 +0800

    [ADD]to kt

---
+++ soultools/src/main/java/com/test/soultools/tool/log/TLog.kt
@@ -0,0 +1,125 @@
+package com.test.soultools.tool.log
+
+import android.util.Log
+import java.lang.StringBuilder
+
+object TLog {
+    private const val MAX_LENGTH = 4000
+    private const val PARAM = "Param"
+    private const val NULL = "null"
+    private const val V = 0x1
+    private const val D = 0x2
+    private const val I = 0x3
+    private const val W = 0x4
+    private const val E = 0x5
+    private const val A = 0x6
+
+    /**
+     * like log d
+     */
+    @JvmStatic
+    fun d(tag: String, vararg objects: Any?) {
+        printLogImp(D, tag, *objects as Array<out Any>)
+    }
+
+    /**
+     * like log i
+     */
+    @JvmStatic
+    fun i(tag: String, vararg objects: Any?) {
+        printLogImp(I, tag, *objects as Array<out Any>)
+    }
+
+    /**
+     * like log w
+     */
+    @JvmStatic
+    fun w(tag: String, vararg objects: Any?) {
+        printLogImp(W, tag, *objects as Array<out Any>)
+    }
+
+    /**
+     * like log e
+     */
+    @JvmStatic
+    fun e(tag: String, vararg objects: Any?) {
+        printLogImp(E, tag, *objects as Array<out Any>)
+    }
+
+    private fun printLogImp(type: Int, tagStr: String, vararg objects: Any) {
+        val contents = wrapperLogContent(tagStr, *objects)
+        val tag = contents[0]
+        val msg = contents[1]
+        val headString = contents[2]
+        printDefault(type, tag, headString + msg)
+    }
+
+    private fun wrapperLogContent(
+        tagStr: String?,
+        vararg objects: Any
+    ): Array<String> {
+        val stackTrace = Thread.currentThread().stackTrace
+        val callLineIndex = 5
+        val targetElement = stackTrace[callLineIndex]
+        val fileName = targetElement.fileName
+        val methodName = targetElement.methodName
+        var lineNumber = targetElement.lineNumber
+        if (lineNumber < 0) {
+            lineNumber = 0
+        }
+        val tag = tagStr ?: fileName
+        val msg = getObjectsString(*objects)
+        val headString = "($fileName:$lineNumber)#$methodName "
+        return arrayOf(tag, msg, headString)
+    }
+
+    private fun getObjectsString(vararg objects: Any): String {
+        return when {
+            objects.size > 1 -> {
+                val stringBuilder = StringBuilder()
+                stringBuilder.append("\n")
+                for (i in objects.indices) {
+                    val `object` = objects[i]
+                    stringBuilder.append(PARAM).append("[").append(i).append("]").append(" = ")
+                        .append(`object`.toString()).append("\n")
+                }
+                stringBuilder.toString()
+            }
+            objects.size == 1 -> {
+                val element = objects[0]
+                element.toString()
+            }
+            else -> {
+                NULL
+            }
+        }
+    }
+
+    private fun printDefault(type: Int, tag: String, msg: String) {
+        var index = 0
+        val length = msg.length
+        val countOfSub = length / MAX_LENGTH
+        if (countOfSub > 0) {
+            for (i in 0 until countOfSub) {
+                val sub = msg.substring(index, index + MAX_LENGTH)
+                printSub(type, tag, sub)
+                index += MAX_LENGTH
+            }
+            printSub(type, tag, msg.substring(index, length))
+        } else {
+            printSub(type, tag, msg)
+        }
+    }
+
+    private fun printSub(type: Int, tag: String, sub: String) {
+        when (type) {
+            V -> Log.v(tag, sub)
+            D -> Log.d(tag, sub)
+            I -> Log.i(tag, sub)
+            W -> Log.w(tag, sub)
+            E -> Log.e(tag, sub)
+            A -> Log.wtf(tag, sub)
+        }
+    }
+
+}
